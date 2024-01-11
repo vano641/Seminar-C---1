b66eba41ac656cc0e83ea8ca458e992c4a88d6dc
Ivan Mysovsky <vano641@mail.ru>
2024-01-11 14:00:44 +0300

    Возведение в степень

---
+++ task0/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number=int.Parse(Console.ReadLine());
+int sqr=number*number;
+Console.WriteLine($"Квадрат числа = {sqr}");
+
+int sqr1=Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа второй способ = {sqr1}");
+
