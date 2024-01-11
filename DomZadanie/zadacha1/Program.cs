332ac74167ba91c5c7f923bc21f4fafa02304628
Ivan Mysovsky <vano641@mail.ru>
2024-01-11 21:16:51 +0300

    ДЗ больше, меньше, равно

---
+++ DomZadanie/zadacha1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+if(A > B)
+{
+    Console.WriteLine("А больше В");
+}
+if(A < B)
+{
+    Console.WriteLine("А меньше В");
+}
+if(A == B)
+{
+    Console.WriteLine("А равно В");
+}
