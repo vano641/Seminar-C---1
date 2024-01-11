656d966319d21c5036cc6345c60a83a37b376e39
Ivan Mysovsky <vano641@mail.ru>
2024-01-11 14:48:41 +0300

    Является ли А квадратом В?

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+if(A==B*B)
+{
+    Console.WriteLine("А это квадрат В");
+}
+else
+{
+    Console.WriteLine("A НЕ квадрат В");
+}
