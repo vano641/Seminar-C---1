695601e816b02239728c39ce3bd980664ae5abf6
Ivan Mysovsky <vano641@mail.ru>
2024-01-11 19:10:35 +0300

    на вход число А, на выходе числа (-А; A)

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int count = -A;
+
+while (count <= A)
+{
+Console.Write($"{count} ");
+count = count + 1;
+}
