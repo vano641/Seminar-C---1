66515aab6dfb744806f33b545739b86f615a42dd
Ivan Mysovsky <vano641@mail.ru>
2024-01-11 16:29:06 +0300

    Дни недели под Номерами

---
+++ task2/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A==1)
+{
+    Console.WriteLine("Понедельник");
+}
+else
+{
+    if(A==2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    else
+    {
+        if(A==3)
+        {
+            Console.WriteLine("Среда");
+        }
+        else
+        {
+            if(A==4)
+            {
+                Console.WriteLine("Четверг");
+            }
+            else
+            {
+                if(A==5)
+                {
+                    Console.WriteLine("Пятница");
+                }
+                else
+                {
+                    if(A==6)
+                    {
+                        Console.WriteLine("Суббота");
+                    }
+                    else
+                    {
+                        if(A==7)
+                        {
+                            Console.WriteLine("Воскресенье");                   
+                        }
+                        else
+                        {
+                            Console.WriteLine("Дня недели с таким номером нет!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
