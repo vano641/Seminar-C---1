c5ce9565ab0d65c7e2811d23e0dd38f5c7c5cd8a
Ivan Mysovsky <vano641@mail.ru>
2024-01-12 14:19:39 +0300

    нахождение max

---
+++ zadacha2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите C: ");
+int C = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if(A > max)
+{
+    max = A;
+}
+if(B > max)
+{
+    max = B;
+}
+if(C > max)
+{
+    max = C;
+}
+// в тренажере надо было написать "return max;" тут, в VScode работать не будет
+Console.WriteLine($"максимальное число = {max}");
