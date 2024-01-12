988863f237d3e5571465025701b4d724483a07eb
Ivan Mysovsky <vano641@mail.ru>
2024-01-12 14:44:27 +0300

    Проверка на Четнось/Нечетность

---
+++ zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A % 2 == 0)
+{
+    Console.WriteLine("Число А - четное.");
+}
+else
+{
+    Console.WriteLine("Число А - НЕ четное");
+}
