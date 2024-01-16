adb6a1cab771f843467562b4ae580036988150fd
Ivan Mysovsky <vano641@mail.ru>
2024-01-16 14:37:24 +0300

    найти max из трех

---
+++ zadacha4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while(count <= A)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+    
+}
