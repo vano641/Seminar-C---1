8ed254847b8caed889efdd97c2ccc466e90aab60
Ivan Mysovsky <vano641@mail.ru>
2024-01-16 19:03:35 +0300

    Поиск элемента в массиве по индексу

---
+++ Lection2/zad3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18} ;
+// индексы      0   1  2   3   4   5   6   7   8
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // остановить алгоритм когда на первом индексе со значением 18
+    }
+    index++;
+}
