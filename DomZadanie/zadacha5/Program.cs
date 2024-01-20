bd576f17e8455b600b7de7e95673756e2305d1eb
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 20:02:08 +0300

    ввести 3ех значное число и вывести вторую цифру этого числа

---
+++ DomZadanie/zadacha5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console; 
+Clear();
+
+Write("Введите трехзначное число А: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100) 
+{
+    WriteLine("вы ввели не Трехзначное число");
+}
+else
+{   
+    if(number > 999)
+    {
+    WriteLine("вы ввели не Трехзначное число");
+    }
+
+        else
+        {
+            int a1 = number/10;
+            int a2 = a1 % 10;
+            WriteLine($"{number} -> {a2}");
+        }
+}        
