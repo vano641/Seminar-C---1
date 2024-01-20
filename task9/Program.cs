2ffe1bf7e4c6be9abfc10a3a8c283433b5786c84
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 18:46:04 +0300

    Проверка А - квадрат B и наоборот

---
+++ task9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите А: ");
+int A = int.Parse(ReadLine());
+Write("Введите B: ");
+int B = int.Parse(ReadLine());
+
+if(A==B*B) // Математический метод вычисления Квадрата
+{
+WriteLine("А это квадрат В");
+}
+else
+{
+    if(Math.Pow(A,2)==B) // Метод встроенных функций вычисления квадрата
+    {
+        WriteLine("B это квадрат A");
+    }
+    else
+    {
+        WriteLine("не квадрат");
+    }
+}
