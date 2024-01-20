bf3c494ff6b1b13a202fd288a267769547814267
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 16:37:10 +0300

    Одновременная проверка 2ух условий

---
+++ task8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console; 
+Clear();
+
+Write("Введите А: ");
+int A = int.Parse(ReadLine());
+// Мое решение (проверяет условия последовательно)
+if(A % 7 == 0)
+{
+    WriteLine("число А кратно 7");
+}
+else
+{
+    WriteLine($"не кратно 7");
+}
+if(A % 23 == 0)
+{
+    WriteLine("число А кратно 23");
+}
+else
+{
+    WriteLine($"не кратно 23");
+}
+
+//Решение Преподавателя
+
+if((A % 7 == 0)&&(A % 23 == 0)) // Одновременная проверка 2ух условий через операто &&
+{
+    WriteLine($"{A} -> Yes ");
+}
+else
+{
+    WriteLine($"{A} -> No ");
+}
