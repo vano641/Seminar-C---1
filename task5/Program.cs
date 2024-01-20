309f0a6eae5c5cca6d508b55642197c64c92ec70
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 13:44:49 +0300

    Случ. число от 10-99 и выбрать большую цифру из этого числа

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console; // Использование функций класса Console,
+// без вызова этого класса в каждой строке.
+Clear();
+int number = new Random().Next(10, 100); // случайное число в диапазое от 10-99 макс 
+// значения не включается в диапазо, поэтому берем 100
+int a1 = number/10; // чтобы взять первую цифру случайного числа 
+int a2 = number %10; // чтобы взять вторую цифру случайного числа
+if(a1>a2)
+{
+    WriteLine($"{number} -> {a1}"); // using static System.Console;=>не нужно писать Console.WriteLine
+}
+else
+{
+    WriteLine($"{number} -> {a2}");
+}
+
