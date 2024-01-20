af072796114058850aab02e208cab1f9c5748a33
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 21:55:42 +0300

    return прерывающий алгоритм в задаче с выходными и рабочими днями недели

---
+++ DomZadanie/zadacha7/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A>7 || A<1)
+{
+    Console.WriteLine("Дня недели с таким номером нет!");
+    return;// если не прекратить работу, при не верновводе, то алгорим продолжит
+}
+if(A > 5) // и напишет в консоли "выходной"
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("рабочий день");
+}
