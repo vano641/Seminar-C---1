6d2225faa6956848b273a29d1ffb1aef64f2f544
Ivan Mysovsky <vano641@mail.ru>
2024-01-20 14:28:56 +0300

    Слуx. 3ех значн. число, вывести первую и третью цифру числа

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using static System.Console; 
+Clear();
+int number = new Random().Next(100, 1000);
+int a1 = number/100;
+int a2 = number%10;
+WriteLine($"{number} -> {a1}{a2}");// первый способ через доп. переменные а1,а2
+WriteLine($"{number} -> {(number / 100) * 10 + number % 10}");//способ без доп. переменных(в 5 строк)
