f570d7cf0dd3056d1ec0e8cdb402acd7eb5f7709
Ivan Mysovsky <vano641@mail.ru>
2024-01-23 19:58:22 +0300

    Вводим координаты Х и У получаем номер четверти плоскости

---
+++ task10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите координату Х: ");
+int x = int.Parse(ReadLine());
+Write("Введите координату Y: ");
+int y = int.Parse(ReadLine());
+
+if(x>0&&y>0)
+{
+    WriteLine("1ая четверть");
+}
+if(x<0&&y>0)
+{
+    WriteLine("2ая четверть");
+}
+if(x<0&&y<0)
+{
+    WriteLine("3ая четверть");
+}
+if(x>0&&y<0)
+{
+    WriteLine("4ая четверть");
+}
