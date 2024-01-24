304ed9e0a0cf103ba6d0f55fa1c61a494819c46e
Ivan Mysovsky <vano641@mail.ru>
2024-01-24 22:00:32 +0300

    таблица возведения числа в 3ю степень

---
+++ DomZadanie/zadacha10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+Clear();
+// Мое Решение
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+
+
+for(int i=1; i<=N; i++) 
+{
+    WriteLine($"{i*i*i} ");
+}
