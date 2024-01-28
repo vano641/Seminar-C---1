4b7e49728df6720636eeddc112b296600c797c17
Ivan Mysovsky <vano641@mail.ru>
2024-01-28 18:06:32 +0300

    Замена символов в тексте

---
+++ Lection3/Program.cs
@@ -0,0 +1,37 @@
+﻿//====== ЗАДАЧА 1 "Работа с Текстом"
+//Дан текст. В тексте нужно Все ПРОБЕЛЫ заменить ЧЕРТОЧКАМИ,
+//маленькие буквы "к" заменить большими "К",
+// Большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012345
+// s[3] = r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        //если i совпало со старым значением, то положить туда новое значение
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";// в другом случае оставить все как есть
+    }
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+
+Console.WriteLine();// отделим задиния друд от друга
+
+newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText);
+
+Console.WriteLine();// отделим задиния друд от друга
+
+newText = Replase(text, 'с', 'С');
+Console.WriteLine(newText);
