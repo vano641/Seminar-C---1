f73168e3570028fcaa8f02dec9e50eb8516a6651
Ivan Mysovsky <vano641@mail.ru>
2024-01-28 21:56:07 +0300

    Вводим N находим сумму цифр от 1 до N с помощью МЕТОДА

---
+++ task14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+int summa = GetSumNumbers(N);
+WriteLine($"Сумма = {summa}");
+
+
+int GetSumNumbers(int number) // в метод передаем число number, метод возвращает сумму
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i++)
+    {
+        sum += i; // к сумме прибавляем значение i
+    }
+    return sum;
+}
