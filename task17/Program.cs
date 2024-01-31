3ff30f78f816353c6418e1704fbb2620421bc5b0
Ivan Mysovsky <vano641@mail.ru>
2024-01-31 17:06:52 +0300

    Создание произвольного бинарного МАССИВА из 0 и 1

---
+++ task17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console; 
+Clear();
+
+WriteLine($"{String.Join(",",GetBunaryArray(8))}" );// метод  String.Join позволяет вывести элементы массива 
+//любой размерности с любым разделителем, в качестве разделителя "," в качестве массива GetBunaryArray(8)
+
+int[] GetBunaryArray(int size) // метод создает массив целочисленных значений
+// int size универсальный метод чтобы формировать массив произвольной длинны
+{
+    int[] result = new int[size];//создаем возвращаемый массив result, [size] - размер массива
+    for(int i = 0; i < size; i++) // т.к индексы в массиве нумеруются с 0 то i < size.
+    // если поставить i <=size то последний элемент массива будет вне цикла
+    {
+        result[i] = new Random().Next(0,2);// обращаемя к элементу[i] массива result и создаем ее случайно
+        // min 0 max на 1 больше => 2
+    }
+    return result;
+}
