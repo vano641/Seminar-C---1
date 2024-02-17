30c9d252ffaa8180c2601af08b193a8f70f127ee
Ivan Mysovsky <vano641@mail.ru>
2024-02-17 19:10:27 +0300

    посчитать сколько положительных чисел в введенном массиве

---
+++ DomZadanie/zadacha17/Program.cs
@@ -0,0 +1,45 @@
+﻿//Ввести с клавиатуры N чисел.
+//Посчитать сколько чисел больше 0 ввёл пользователь
+//0, 7, 8, -2, -2 => 2
+//1, -7, 567, 89, 223 => 3
+
+using System;
+using static System.Console;
+Clear();
+
+
+//1
+Write("Введите массив через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine(String.Join(" ", array));
+
+//2
+int PCount = PositivCount(array);
+WriteLine($"Количество элементов > 0 = {PCount}");
+
+
+//1
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
+
+int PositivCount(int[] inArray)
+{
+    int count = 0;
+    foreach (int item in inArray)
+    {
+        if(item > 0)
+        {
+            count++;
+        }   
+    }
+    return count;
+}
