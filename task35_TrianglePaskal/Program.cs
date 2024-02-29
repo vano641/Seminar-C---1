e4e047164bf79d7a019bdc20efe1a3cf0daf7737
Ivan Mysovsky <vano641@mail.ru>
2024-02-29 15:05:57 +0300

    Вывод Треугольника Паскаля

---
+++ task35_TrianglePaskal/Program.cs
@@ -0,0 +1,48 @@
+﻿// Показать треугольник Паскаля
+// Сделать вывод в виде равнобедренного треугольника
+//      1
+//     1 1
+//    1 2 1
+//   1 3 3 1
+//  1 4 6 4 1
+//1 5 10 10 5 1
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите количество строк: ");
+int rows = int.Parse(ReadLine());
+
+PrintTriangle(rows);
+
+//2 Метод выводящий треугольник
+void PrintTriangle(int n) // входной параметр - количество строк в нашем треугольнике
+{
+    for (int i = 0; i < n; i++)
+    {
+        // в первом цикле выводим Пробелы
+        for (int c = 0; c <= (n - i); c++)
+        {
+            Write("  ");
+        }
+        //Вторым циклом вычисляем число которое расположено в рамках Треугольника Паскаля
+        for (int k = 0; k <= i; k++)
+        {
+            Write("   ");
+            Write(factorial(i)/(factorial(k)*factorial(i-k)));
+        }
+        WriteLine();
+    }
+}
+
+//1 Метод вычисляющий Факториал числа
+int factorial(int n)
+{
+    int f = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
