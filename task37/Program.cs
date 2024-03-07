7faede8d05515e2139004c14d9aff54604983d43
Ivan Mysovsky <vano641@mail.ru>
2024-03-07 12:24:11 +0300

    Диапазон чисел от M - N с помощью рекурсии

---
+++ task37/Program.cs
@@ -0,0 +1,24 @@
+﻿// РЕКУРСИВНЫЙ МЕТОД. 
+// Задайте значения M и N. 
+// Вывести все натуральные числа в промежутке от M до N
+// M = 4
+// N = 8 => 4,5,6,7,8
+
+using System;
+using static System.Console;
+Clear();
+
+Write("введите M: ");
+int m = int.Parse(ReadLine());
+
+Write("введите N: ");
+int n = int.Parse(ReadLine());
+WriteLine(PrintNumber(m,n));
+
+string PrintNumber(int start, int end)
+{
+    if(start == end) return start.ToString();
+    
+    return(start + " " + PrintNumber(start + 1, end));
+    
+}
